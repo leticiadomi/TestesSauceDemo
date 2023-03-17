5f4ef5410bf4f00c04f45ecb1f89df8aaa18776e
Letícia Domingos <leticiabhz@outlook.com>
2023-03-17 14:23:55 -0300

    Criação de ligação do appsetings c/ o código.

--- TestesSauceDemo/Metodos/InicializaDriver.cs
+++ TestesSauceDemo/Metodos/InicializaDriver.cs
@@ -1,21 +1,27 @@
-﻿
+﻿using Microsoft.Extensions.Configuration;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Support.UI;
 using System.Configuration;
+using Microsoft.Extensions.Configuration.Json;
 
 namespace TestesSauceDemo.Metodos
 {
     [TestClass]
+
     public class InicializaDriver
     {
+        
+
         IWebDriver driver;
         WebDriverWait espera;
 
         [TestMethod]
         public void Inicializar()
         {
-            driver = new ChromeDriver(@"C:\chromedriver_win32");
+            var configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+            //driver = new ChromeDriver(@"C:\chromedriver_win32");
+            driver = new ChromeDriver(configuration.GetValue<string>("DriverChrome"));
             espera = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
 
             driver.Manage().Window.Maximize();
